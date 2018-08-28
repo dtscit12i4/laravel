46cdbd64ac103532f3a820ffd020065a6bf9ec53
Dang Tung Son <son@dactech.vn>
2018-08-28 15:17:00 +0700

    seven commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\AdminUser;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Http\Requests\EditUser;
 
 class UsersController extends Controller
 {
@@ -21,49 +21,6 @@ class UsersController extends Controller
 
     public function getdata($id) {
         $user = AdminUser::find($id);
-        // $template = 
-        //  '
-        //     <div class="form-group">
-        //         <label for="name">First Name:</label>
-        //         <input type="text" name="firstname" placeholder="firstname" id="firstname" value="'.$users->firstname.'" class="form-control">
-        //     </div>
-
-        //     <div class="form-group">
-        //         <label for="name">Last Name:</label>
-        //         <input type="text" name="lastname" placeholder="lastname" id="lastname" value="'.$users->lastname.'" class="form-control">
-        //     </div>
-
-        //     <div class="form-group">
-        //         <label for="email">Login Name:</label>
-        //         <input type="text" name="login_name" placeholder="login_name" id="login_name" value="'.$users->login_name.'" class="form-control" readonly>
-        //     </div>
-
-        //     <div class="form-group">
-        //         <label for="password">Password:</label>
-        //         <input type="password" name="password" placeholder="Password" id="password" class="form-control">
-        //     </div>
-
-        //     <div class="form-group">
-        //         <label for="password_confirmation">Confirm Password:</label>
-        //         <input type="password" name="password_confirmation" placeholder="Confirm Password" id="password_confirmation" class="form-control">
-        //     </div>';
-
-        // if ( $users->role == 1) {
-        //     $template .= '<div class="form-group">
-        //         <label for="address">Role:</label>
-        //         Admin <input type="radio" name="role" value="1" checked />
-        //         NormalUser <input type="radio" name="role" value="0" />
-        //         </div>
-        //     ';
-        // }
-        // else {
-        //     $template .= '<div class="form-group">
-        //         <label for="address">Role:</label>
-        //         Admin <input type="radio" name="role" value="1" />
-        //         NormalUser <input type="radio" name="role" value="0" checked />
-        //         </div>
-        //     ';
-        // }
 
         return view('admin.users.edit1', ['user' => $user]);
     }
@@ -102,20 +59,17 @@ class UsersController extends Controller
         return view('admin.users.edit', ['users' => $users]);
     }
 
-    public function update(Request $request, $id) {
+    public function update(EditUser $request, $id) {
         $user = AdminUser::find($id);
         // Validate the user
-        $request->validate([
-           'firstname' => 'required',
-           'lastname' => 'required',
-        ]);
+        // $request->validate([
+        //    'firstname' => 'required',
+        //    'lastname' => 'required',
+        // ]);
         $user->firstname = $request->firstname;
         $user->lastname = $request->lastname;
         $user->role = $request->role;
         if ($request->password) {
-            $request->validate([
-                'password' => 'required|confirmed',
-            ]);
             $user->password = bcrypt($request->password);
         }
         // Save the data
